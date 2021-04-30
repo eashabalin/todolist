fec57a023a2814b6062cdb783a252141be03fa3a
Egor Shabalin <eashabalin.1@gmail.com>
2021-04-30 21:57:43 +0300

    Readme has been added

--- ToDoList/ToDo.swift
+++ ToDoList/ToDo.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct ToDo: Equatable {
+    let id = UUID()
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "To-Do One", isComplete: false, dueDate: Date(), notes: "Notes 1")
+        let todo2 = ToDo(title: "To-Do Two", isComplete: false, dueDate: Date(), notes: "Notes 2")
+        let todo3 = ToDo(title: "To-Do Three", isComplete: false, dueDate: Date(), notes: "Notes 3")
+        return [todo1, todo2, todo3]
+    }
+}
+
+
+
